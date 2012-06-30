b6851457652414762b94f3c84b716d92c79ba6f2
Hamilton Oliveira <jhamilton.oliveira@gmail.com>
2012-06-29 21:25:33 -0300

    Registro de ponto, ficha de ponto

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,7 +5,7 @@ class ReportsController < ApplicationController
 
 	def ficha_ponto
 		employee = Employee.find_by_registry(params[:registry])
-        card_points = Array.new(100)
+        card_points = []
         current_date_time = nil
 		cp = nil
 		report = ODFReport::Report.new("#{Rails.root.to_s}/app/reports/ficha_ponto.odt") do |r|
@@ -17,37 +17,35 @@ class ReportsController < ApplicationController
 			@points = Point.order("date_time").where("date_time between ? and ? and employee_id = ?", params[:date_start], params[:date_end], employee.id)
 
 			
-			
-            @points.each do |p|
-            	if current_date_time == nil or current_date_time.to_date != p.date_time.to_date
-            		card_points << cp unless current_date_time.nil?
-            		current_date_time = p.date_time
-            		cp = CardPoint.new
-            		cp.date_time = p.date_time.to_date
-            	end
-            	case p.action 
-            	when 1 then cp.e1 = p.date_time.to_time
-            	when 2 then cp.s2 = p.date_time.to_time
-            	when 3 then cp.e3 = p.date_time.to_time
-            	when 4 then cp.s4 = p.date_time.to_time
-            	when 5 then cp.e5 = p.date_time.to_time
-            	when 6 then cp.s6 = p.date_time.to_time
-            	end
+			@points.each do |p|
+                if current_date_time == nil or current_date_time.to_date != p.date_time.to_date
+                    card_points << cp unless current_date_time.nil?
+                    current_date_time = p.date_time
+                    cp = CardPoint.new
+                    cp.date_time = p.date_time.strftime("%d/%m/%Y")
+                end
+
+                case p.action
+                  when 1 then cp.e1 = p.date_time.strftime("%R")
+                  when 2 then cp.s2 = p.date_time.strftime("%R")
+                  when 3 then cp.e3 = p.date_time.strftime("%R")
+                  when 4 then cp.s4 = p.date_time.strftime("%R")
+                  when 5 then cp.e5 = p.date_time.strftime("%R")
+                  when 6 then cp.s6 = p.date_time.strftime("%R")
+                end
+
             end
 
             card_points << cp
 
-            puts card_points
-
-            r.add_table("POINTS", card_points, :header => true) do |t|
+            r.add_table("TABLE_POINTS", card_points, :header => true, :skip_if_empty => true) do |t|
             	t.add_column(:date_time)
-            	t.add_column(:e1)
-            	t.add_column(:s2)
-            	t.add_column(:e3)
+                t.add_column(:e1)
+                t.add_column(:s2)
+                t.add_column(:e3)
                 t.add_column(:s4)
                 t.add_column(:e5)
-                t.add_column(:s6)
-                t.add_column(:obs) 
+                t.add_column(:s6) 
             end
 		end
 
