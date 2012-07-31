1c0f91f0bce6a769a4b569f172eba5d967273071
Hamilton Oliveira <jhamilton.oliveira@gmail.com>
2012-07-31 10:58:39 -0300

    JasperReport habilitado

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Ponto::Application.initialize!
+
+ENV['JAVA_HOME'] = "/usr/java/jdk1.6.0_33/"
+ENV['LD_LIBRARY_PATH'] = "#{ENV['LD_LIBRARY_PATH']}:#{ENV['JAVA_HOME]}/jre/lib/i386:#{ENV['JAVA_HOME']}/jre/lib/i386/client"
