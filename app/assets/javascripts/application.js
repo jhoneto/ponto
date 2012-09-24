d2043edf37e62f656745aba3cfcfdd34d88c40bb
Desenvolvimento <desenvolvimento@railsapps.(none)>
2012-09-24 12:11:31 -0300

    Erro de CSS e JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,12 +4,13 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
 //= require jquery_nested_form
 //= require jquery.maskedinput
+
+//= require bootstrap
 
 
 $(function() {
