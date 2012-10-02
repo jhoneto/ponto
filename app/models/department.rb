e22cb30af319eb2e7f843426e5e3ec8fff4e09b3
brunolopesjn <brunolopesjn@gmail.com>
2012-10-02 10:39:45 -0300

    Alterado o foco do textbox na tela de login
    - Colocado o foco automático no campo login
    - Retirado o texto padrão dos campos de login e senha da tela de login

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,13 +1,13 @@
 class Department < ActiveRecord::Base
   belongs_to :enterprise, :class_name => "Enterprise", :foreign_key => "enterprise_id"
   belongs_to :employee, :class_name => "Employee", :foreign_key => "employee_id"
-	validates_presence_of :name
-	validates_presence_of :enterprise_id
-	validates_presence_of :employee_id
-	scope :by_name, lambda {|parameter| where("upper(name) like upper(?)", "%#{parameter}%")}
-	
-	
-	def to_xml(options = {})
+  validates_presence_of :name
+  validates_presence_of :enterprise_id
+  validates_presence_of :employee_id
+  scope :by_name, lambda {|parameter| where("upper(name) like upper(?)", "%#{parameter}%")}
+
+
+  def to_xml(options = {})
     options[:indent] ||= 2
     xml = options[:builder] ||= Builder::XmlMarkup.new(:indent => options[:indent])
     xml.instruct! unless options[:skip_instruct]
@@ -17,6 +17,6 @@ class Department < ActiveRecord::Base
       xml.boss self.employee.name unless self.employee_id.nil?
     end
   end
-  
-  
+
+
 end
